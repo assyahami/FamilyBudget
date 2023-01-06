3e1799daaded06c6be729626ae8d58affa73a387
assyahame <ahamedashiq08@gmail.com>
2023-01-06 14:49:46 +0530

    product controller updating

--- Back-End/controller/products.js
+++ Back-End/controller/products.js
@@ -18,8 +18,8 @@ const addProducts = async (req, res) => {
     })
 }
 
-const updateProducts = (req, res) => {
-    const updateProduct = await products.findOneAndUpdate(req.body)
+const updateProducts = async (req, res) => {
+    const updateProduct = await products.create(req.body)
     res.status(200).json({
         suceess: true,
         body: "update products"
